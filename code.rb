b98b31e6675e67f27609023e809e5583c3ce5524
Seth Thompson <seth.h.thompson@gmail.com>
2017-04-12 09:53:51 -0400

    Refactored final step

--- code.rb
+++ code.rb
@@ -15,25 +15,17 @@ end
 
 CSV.foreach("ingredients.csv", headers: false) do |row|
   db_connection do |conn|
-    insert_ingredient = "INSERT INTO ingredients_table (ingredients_place, ingredients_name) VALUES ($1, $2)"
-    values = [row[0], row[1]]
+    insert_ingredient = "INSERT INTO ingredients_table (name) VALUES ($1)"
+    values = [row[1]]
     conn.exec_params(insert_ingredient, values)
   end
 end
 
-select_ingredients = "SELECT ingredients_place, ingredients_name FROM ingredients_table"
+select_ingredients = "SELECT * FROM ingredients_table"
 ingredients = db_connection do |conn|
   conn.exec(select_ingredients)
 end
 
-ingredients_array = []
 ingredients.to_a.each do |ingredient|
-  ingredient.each do |key, value|
-    ingredients_array << value
-  end
+  puts "#{ingredient["id"]}. #{ingredient["name"]}"
 end
-
-puts "#{ingredients_array[0]}. #{ingredients_array[1]}"
-puts "#{ingredients_array[2]}. #{ingredients_array[3]}"
-puts "#{ingredients_array[4]}. #{ingredients_array[5]}"
-puts "#{ingredients_array[6]}. #{ingredients_array[7]}"
--- schema.sql
+++ schema.sql
@@ -2,6 +2,5 @@ DROP TABLE IF EXISTS ingredients_table;
 
 CREATE TABLE ingredients_table (
   id SERIAL PRIMARY KEY,
-  ingredients_place VARCHAR(255),
-  ingredients_name VARCHAR(255)
+  name VARCHAR(255)
 );
